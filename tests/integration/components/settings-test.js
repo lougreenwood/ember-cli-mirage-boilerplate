222acd38d3d6e55a94b1c3e9efb457360c4ff907
Lou Greenwood <lou.greenwood@leadfeeder.com>
2019-10-23 15:19:23 +0100

    example of passing & failing usage

--- tests/integration/components/settings-test.js
+++ tests/integration/components/settings-test.js
@@ -6,7 +6,21 @@ module("Integration | Component | settings", function(hooks) {
   setupRenderingTest(hooks);
   setupMirage(hooks);
 
-  test("it renders", async function() {
+  test("server.create singular model passes", async function(assert) {
+    // With incorrect model name (actual model name is `settings`)
+    this.server.create("setting");
+    assert.ok(
+      true,
+      "server.create() works for incorrect, singular version of model"
+    );
+  });
+
+  test("server.create singular model fails", async function(assert) {
+    // With correct model name, which is actually plural
     this.server.create("settings");
+    assert.ok(
+      true,
+      "server.create() fails for actual, plural version of model"
+    );
   });
 });
